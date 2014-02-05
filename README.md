54bb9f697ee70645b5ef9a5254612ad23e5159a1
Matt Smith <msmith@vmfoundry.com>
2014-02-04 22:37:47 -0600

    Update readme. Add initial install script.
    Signed-off-by: Matt Smith <msmith@vmfoundry.com>

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ aws-es-setup
 ============
 
 AWS Elasticsearch Config
+A basic Elasticsearch installation and configuration bash script. Won't be maintained for long... just monkeying around.
---
+++ es-install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd ~
+
+# Config
+ES_VERSION="elasticsearch-1.0.0.RC2.noarch.rpm"
+AWS_PLUGIN_VERSION="2.0.0.RC1"
+
+# Install ES
+echo "Installing Elasticsearch"
+wget https://download.elasticsearch.org/elasticsearch/elasticsearch/$ES_VERSION
+sudo rpm -ivh elasticsearch-1.0.0.RC2.noarch.rpm
+echo "Elasticsearch Installed"
+
+# Install plugins (AWS and Marvel)
+sudo /usr/share/elasticsearch/bin/plugin -i elasticsearch/elasticsearch-cloud-aws/$AWS_PLUGIN_VERSION
+sudo /usr/share/elasticsearch/bin/plugin -i elasticsearch/marvel/latest
+
+# If a custom config file exists
+if [ -f ./elasticsearch.yaml ]
+  then
+  	echo "Adding our custom config"
+    sudo rm /etc/elasticsearch/elasticsearch.yml
+    sudo cp ./elasticsearch.yaml /etc/elasticsearch/elasticsearch.yml
+fi
+
+echo "Cleaning up..."
+rm elasticsearch-1.0.0.RC2.noarch.rpm
